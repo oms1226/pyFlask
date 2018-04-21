b8c58af77bf8da9bc835bb97c9670b8d7a5558a6
oms1226 <oms1226@gmail.com>
2018-04-21 16:44:03 +0900

    20180421 오후에 selenium naver id, pw 입력하고 form tag summit으로 접속하는 간단 접속 예제 추가

--- py_crawler/run.py
+++ py_crawler/run.py
@@ -5,16 +5,23 @@ from selenium import webdriver as wd
 # 타켓 사이트 지정 (PC 웹, 모바일웹)
 target_url = 'http://tour.interpark.com/'
 
+
 # 브라우져 띠우기
 # 에이전트 변조, 프록시 우회, 이미지는 제외하겠금 로드
 driver = wd.Chrome(executable_path='./chromedriver.exe')
 
-# 페이지로드
-driver.get( target_url )
+if False:
+    # 페이지로드
+    driver.get( target_url )
+    # 검색창으로 이동한다. #검색창은 크롬에서 페이지 검사로 찾는다.
+    # 검색창에서 파리로 입력한다.
+    driver.find_element_by_id('SearchGNBText').send_keys('파리')
 
-# 검색창으로 이동한다. #검색창은 크롬에서 페이지 검사로 찾는다.
-# 검색창에서 파리로 입력한다.
-driver.find_element_by_id('SearchGNBText').send_keys('파리')
+    # 검색 버튼 클릭 #검색 버튼이 없다면, form tag에 summit으로 가면 된다.
+    driver.find_element_by_class_name('search-btn').click()
 
-# 검색 버튼 클릭 #검색 버튼이 없다면, form tag에 summit으로 가면 된다.
-driver.find_element_by_class_name('search-btn').click()
+#form tag에 summit으로 로그인 하는 예제
+driver.get( 'https://www.naver.com/' )
+driver.find_element_by_id('id').send_keys('id')
+driver.find_element_by_id('pw').send_keys('pw')
+driver.find_element_by_id('frmNIDLogin').submit()
