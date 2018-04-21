f66792a06b064531e4df1e2ff639036ab7d51798
oms1226 <oms1226@gmail.com>
2018-04-21 13:48:57 +0900

    20180421 오후에 blueprint config app_config class로 로딩 작업 완료

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # pyFlask
+
+## pythondb.sql
+### https://support.appliedi.net/kb/a1034/how-to-import-and-export-a-mysql-database-using-heidisql.aspx
+### 아래 링크를 참조해서 HeidiSQL을 통해서 export를 한 파일임
+### id:root / pwd 0000
--- py_blueprint/service/__init__.py
+++ py_blueprint/service/__init__.py
@@ -6,8 +6,15 @@ def create_app( config_path='resource/config.cfg' ):
     app = Flask(__name__)
     #=======================================================================
     # 환경 변수 로드 => 파일로 부터, 클래스로 정의된 환경변수 객체로부터
+    # 파일로 부터 로드, silent=True 는 습관적으로 쓴다고 한다. 정확한 이유는 모르겠다.
     app.config.from_pyfile( config_path, silent=True )
-    print( app.config.items() )
+    # 객체로 부터 로드
+    from service.app_config import WebConfig
+    app.config.from_object( WebConfig )
+    print( app.config.items() )#출력결과:#dict_items([('DEBUG', False), ('TESTING', False), ('PROPAGATE_EXCEPTIONS', None), ('PRESERVE_CONTEXT_ON_EXCEPTION', None), ('SECRET_KEY', None), ('PERMANENT_SESSION_LIFETIME', datetime.timedelta(31)), ('USE_X_SENDFILE', False), ('LOGGER_NAME', 'service'), ('LOGGER_HANDLER_POLICY', 'always'), ('SERVER_NAME', None), ('APPLICATION_ROOT', None), ('SESSION_COOKIE_NAME', 'session'), ('SESSION_COOKIE_DOMAIN', None), ('SESSION_COOKIE_PATH', None), ('SESSION_COOKIE_HTTPONLY', True), ('SESSION_COOKIE_SECURE', False), ('SESSION_REFRESH_EACH_REQUEST', True), ('MAX_CONTENT_LENGTH', None), ('SEND_FILE_MAX_AGE_DEFAULT', datetime.timedelta(0, 43200)), ('TRAP_BAD_REQUEST_ERRORS', False), ('TRAP_HTTP_EXCEPTIONS', False), ('EXPLAIN_TEMPLATE_LOADING', False), ('PREFERRED_URL_SCHEME', 'http'), ('JSON_AS_ASCII', True), ('JSON_SORT_KEYS', True), ('JSONIFY_PRETTYPRINT_REGULAR', True), ('JSONIFY_MIMETYPE', 'application/json'), ('TEMPLATES_AUTO_RELOAD', None), ('TEST_MODE', True), ('TEST_URL', '127.0.0.1')])
+    print( app.config.keys() )
+    print( app.config.values() )
+    print( 'DB_URL:', app.config['DB_URL'])
     # 디비 로드 => 디비와 연결등등...
     # blueprint 생성 및 연결 작업 => 컨트롤러
 
