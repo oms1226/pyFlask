b7c3b9a09d6dbd45aca22f7d79de7b163ed76622
oms1226 <oms1226@gmail.com>
2018-04-21 15:47:33 +0900

    20180421 오후에 blueprint user orm update 간략 작업 코드 추가

--- py_blueprint/service/controller/user.py
+++ py_blueprint/service/controller/user.py
@@ -15,10 +15,20 @@ def root():
     print('회원 서비스 메인')
     return '회원 서비스 메인'
 
-@blueprintUser.route('/join/<name>/<password>')
+@blueprintUser.route('/join/<name>/<password>')#http://127.0.0.1:81/users/join/test/testpwd
 def join(name, password):
     print('회원 서비스 가입')
     newUser = User( name, password )
     dao.add( newUser )
     dao.commit()
     return '회원 서비스 가입'
+
+@blueprintUser.route('/update/<_name>/<newname>')#http://127.0.0.1:81/users/update/test/newtest
+def update(_name, newname):
+    print('회원 서비스 수정')
+    #해당 이름을 가진 데이터를 획득
+    findUser = dao.query(User).filter_by(name=_name).first()
+    print ( findUser )
+    findUser.name = newname
+    dao.commit()
+    return '회원 서비스 수정'
